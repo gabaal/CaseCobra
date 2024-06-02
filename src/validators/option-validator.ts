ddf606e7e7b038c0d327caf7a895daf00dc34e4b
gerald <geraldbaalham@live.co.uk>
2024-06-02 13:08:55 +0100

    Case color options complete

---
+++ src/validators/option-validator.ts
@@ -0,0 +1,17 @@
+// bg-blue-950 border-blue-950
+// bg-zinc-900 border-zinc-900
+// bg-rose-950 border-rose-950
+// bg-white-950 border-white-950
+// bg-yellow-300 border-yellow-300
+
+export const COLORS = [
+  { label: "Black", value: "black", tw: "zinc-900" },
+  {
+    label: "Blue",
+    value: "blue",
+    tw: "blue-950",
+  },
+  { label: "Rose", value: "rose", tw: "rose-950" },
+  { label: "White", value: "white", tw: "white-950" },
+  { label: "Yellow", value: "yellow", tw: "yellow-300" },
+] as const;
